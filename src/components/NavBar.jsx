899f596cf4b93ac390c43cc1fecae0d1ac31b5e1
Mohammed Segval <mohammedsegval53@gmail.com>
2023-03-16 11:13:54 -0400

    added responsive navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
+import menu from '../assets/images/menu.svg'
+import PopupNav from './PopupNav'
 
 const NavBar = ({navlinks}) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  const toggleMenu = () => setMenuIsOpen(!menuIsOpen)
+
   return (
     <>
-      <header>
-        <nav>
-          <NavLink to={'/'} className=''>
-            <img src={logo} alt="logo" className='' />
+      <header className='flex items-center justify-center w-auto h-auto absolute top-7 left-0 right-0'>
+        <nav className='flex items-center justify-between travigo-container'>
+          <NavLink to={'/'} className='flex items-center'>
+            <img src={logo} alt="logo" className='w-22 h-9 object-contain' />
           </NavLink>
-          <ul>
+          <ul className='flex items-center gap-7 lg:hidden'>
             {navlinks?.map((val,i) => (
-              <li>
+              <li key={i} className='text-lg text-slate-900'>
                 <NavLink to={'#'} className=''>{val.link}</NavLink>
               </li>
             ))}
-            <li>
-              <button type='button'>Join Us</button>
+          </ul>
+          <ul className='flex items-center lg:hidden'>
+          <li>
+              <button type='button' className='button-emrald text-base px-0'>Join Us</button>
             </li>
           </ul>
+          <ul className='hidden lg:flex items-center'>
+            <li>
+              <button 
+                type='button'
+                onClick={toggleMenu}
+                className='flex items-center justify-center transition-all duration-200 active:scale-50'
+              >
+                <img src={menu} alt="" className='object-cover shadow-sm filter'/>
+              </button>
+            </li>
+          </ul>
+          {menuIsOpen ? <PopupNav navlinks={navlinks}/> : ''}
         </nav>
       </header>
     </>
